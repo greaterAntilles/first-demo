85be7c60e1c26640e3ab86d0e0a652d2f77feef0
antilles <antilles183@gmail.com>
2021-06-21 14:16:39 -0400

    ubuntu changes
    Getting my ubuntu system set up

--- helloWorld.py
+++ helloWorld.py
@@ -1,8 +1,29 @@
-
 def main():
-	str = "hello world!"
-	print(str)
+    str = "hello world!"
+    print(str)
+
+    print( myFun() )
+
+
+def myFun():
+    print("This is myFun()")
+    num1, num2 = getNumbers()
+    result = addNumbers(num1, num2)
+
+    return result
+
+
+def getNumbers() -> tuple:
+    num1 = float(input("Number 1: "))
+    num2 = float(input("Number 2: "))
+    nums = (num1, num2)
+    print("in getNumbers: ", type(nums) )
+    return nums
+
+
+def addNumbers(x, y) -> float:
+    return x + y
 
 
 if __name__ == "__main__":
-	main()
+    main()
